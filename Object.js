a91fc237acfcbd88fd49d1f2b6d69ac2c0f4d2c4
Akhill123 <Akhill123>
2023-01-25 17:39:58 +0530

    object code

---
+++ Object.js
@@ -0,0 +1,22 @@
+
+//object creation
+//normal object
+let akhill={
+    age: 24, emailid: "akkiakkik@gmail.com" 
+}
+console.log(akhill,akhill.age)
+
+//array object
+let akhill1=["akki","akhilu","akill"]
+console.log(akhill1, akhill1[0])
+
+//date object
+let dob=new Date("07-01-1999")
+console.log(dob)
+
+var obj={
+    key:"val",
+    k2:"ahjchada"
+}
+console.log(obj)
+console.log(obj.k2)
